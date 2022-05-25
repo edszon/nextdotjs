4af0b31a68799e1bcc68c48e07a5ba274e43b14a
edszon <leonixgamerbr@gmail.com>
2022-05-25 20:30:18 -0300

    deletando pagina de login pq ta quebrada

--- pages/login.js
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head'
-
-<Head>
-        <title>Login</title>
-        <meta name="description" content="Exemplo pagina de login" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      
